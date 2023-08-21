754b516b3aae94a1e680b68dcbecdc198234cc8a
Lucas Sanches <sancheseducacional@gmail.com>
2023-08-21 20:29:04 -0300

    exercicio em java

---
+++ ex/hello.java
@@ -0,0 +1,5 @@
+public class CursoGit{
+	public static void public static void main(String[] args) {
+		System.out.println("Olá, GitHub. Esse é o meu primeiro arquivo em .java na plataforma!")
+	}
+}
