48e74ec7638f14dc64fcaaf51075cbbb2ab52e4a
Lucas Sanches <sancheseducacional@gmail.com>
2023-08-21 21:05:16 -0300

    Revert "java e revert"
    This reverts commit 60324a28db97724809d0baf0e5e03a86185fa186.

--- ex/hello.java
+++ ex/hello.java
@@ -1,7 +1,5 @@
 public class CursoGit{
 	public static void public static void main(String[] args) {
-		System.out.println("Olá, GitHub. Esse é o meu primeiro arquivo em .java na plataforma!");
-		testando o git revert
-
+		System.out.println("Olá, GitHub. Esse é o meu primeiro arquivo em .java na plataforma!")
 	}
 }
