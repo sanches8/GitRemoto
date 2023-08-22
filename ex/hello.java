60324a28db97724809d0baf0e5e03a86185fa186
Lucas Sanches <sancheseducacional@gmail.com>
2023-08-21 21:03:30 -0300

    java e revert

--- ex/hello.java
+++ ex/hello.java
@@ -1,5 +1,7 @@
 public class CursoGit{
 	public static void public static void main(String[] args) {
-		System.out.println("Olá, GitHub. Esse é o meu primeiro arquivo em .java na plataforma!")
+		System.out.println("Olá, GitHub. Esse é o meu primeiro arquivo em .java na plataforma!");
+		testando o git revert
+
 	}
 }
